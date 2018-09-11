f004b9e043460a00f61133c68edbd36993282612
nielseni6 <36169018+nielseni6@users.noreply.github.com>
2018-09-10 21:35:20 -0400

    Update math.h

--- Assignment/math.h
+++ Assignment/math.h
@@ -19,11 +19,74 @@
  * ^ Bitwise XOR (num1 ^ num2)
  * ~ Bitwise Inverse (~num1) for this operation, num1 and num2 are still needed
 */
-
+#include <stdio.h>
 #ifndef MATH_H_
 #define MATH_H_
 
+
 //Part of your documentation should be listing the valid inputs and outputs for the functions you create.
-int math(int num1, int num2, char Operator);
+int math(int num1, int num2, char Op);
+int main()
+{
+    int n1 = 8;     // First value to be entered into the math function
+    int n2 = 4;     // Second value to be entered into the math function
+    char Op = '-';  // Operation to be entered into math function
+    int num3;       // Placeholder variable to accept math function output
+    
+    //code below allows user to enter their own values for calculation
+    printf("Enter first number: \n");   // asking for 1st number
+    scanf("%d", &n1);                   // setting n1 to first answer
+    printf("Enter second number: \n");  // asking for 2nd number
+    scanf("%d", &n2);                   // setting n2 to second answer
+    printf("Enter operator: \n");       // asking for operation to be done
+    scanf("%c", &Op);                   // setting Op to third answer
+    
+    num3 = math(n1, n2, Op); // sets variable num3 to value determined by math function
+    printf("%d",num3); // Printing value determined by math function
+}
+int math(int num1, int num2, char Op){
+    // The switch function checks the operation and returns the answer based on the operation and the numbers entered into math
+    // If none of the operations match then it will return num1
+    switch(Op){
+        case '+': 
+            return num1+num2;
+            break;
+        case '-':
+            return num1-num2;
+            break;
+        case '*': 
+            return num1*num2;
+            break;
+        case '/':
+            return num1/num2;
+            break;
+        case '%': 
+            return num1%num2;
+            break;
+        case '<':
+            return num1<<num2;
+            break;
+        case '>': 
+            return num1>>num2;
+            break;
+        case '&':
+            return num1&num2;
+        case '|': 
+            return num1|num2;
+            break;
+        case '^':
+            return num1^num2;
+            break;
+        case '~': 
+            return ~num1;
+            break;
+        default :
+           return num1;
+    }
+        
+    return 0;
+}
+
+ ;
 
 #endif /* MATH_H_ */
